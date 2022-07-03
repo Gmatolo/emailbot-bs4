b95464590a47ca4d3e426e7432cbc5a18a8f754d
gmatolo <geraldmuli7@gmail.com>
2022-07-03 08:55:51 +0300

    Adds scrape news function that returns the enumnerated content in rows
     Changes to be committed:
    	modified:   main.py
     Changes not staged for commit:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from bs4 import Beautiful
+from bs4 import BeautifulSoup
 import requests
 
 content = ''
+print('Wait as we get the most trending news for you')
+def scrape_news(url):
+    print('Wait as we get the most trending news for you')
+    cnt = ''
+    cnt +=('<b>Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        #create rows for enumeration if tag isnt More, else throw a blank
+        cnt += ((str(i+1)+' :: '+ '<a href="' + tag.a.get('href') + '">' + tag.text + '</a>' + "\n" + '<br>') if tag.text!='More' else '')
+        #print(tag.prettify) #find_all('span',attrs={'class':'sitestr'}))
+    return(cnt)
 
+print('Content returned')
