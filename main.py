e160adc3f56857c87ebd28770f792bdf8c2b444a
gmatolo <geraldmuli7@gmail.com>
2022-07-03 09:16:07 +0300

    take url input, format the returned response

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ def scrape_news(url):
     return(cnt)
 
 print('Content returned')
+
+#take url input, format returned response
+cnt = crape_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>--------------<br>')
+content +=('<br><br>This is we have for you')
